f2f45bec3ea1a9ffbb20311950288f7af13ff973
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-12-06 13:36:30 +0100

    CLI

--- rabbitmq/src/main/azure/deploy.sh
+++ rabbitmq/src/main/azure/deploy.sh
@@ -5,8 +5,9 @@ export RNDM=$RANDOM
 
 # Set the environment variables
 export RESOURCE_GROUP=rg-antoniomanug-$RNDM
-export REGION=pick_your_closest_region
-export SERVICE_BUS=sb-antoniomanug-$RNDM
+# Pick your closest region
+export REGION=eastus2
+export SERVICE_BUS_NAMESPACE=sb-antoniomanug-$RNDM
 
 # Create the resource group  
 az group create --name $RESOURCE_GROUP --location $REGION
@@ -14,24 +15,24 @@ az group create --name $RESOURCE_GROUP --location $REGION
 # Create the Service Bus namespace
 az servicebus namespace create \
   --resource-group $RESOURCE_GROUP \
-  --name $SERVICE_BUS \
+  --name $SERVICE_BUS_NAMESPACE \
   --sku Premium \
   --location $REGION    
 
 # Create the Service Bus queues  
 az servicebus queue create \
   --resource-group $RESOURCE_GROUP \
-  --namespace-name $SERVICE_BUS \
+  --namespace-name $SERVICE_BUS_NAMESPACE \
   --name foo
 az servicebus queue create \
   --resource-group $RESOURCE_GROUP \
-  --namespace-name $SERVICE_BUS \
+  --namespace-name $SERVICE_BUS_NAMESPACE \
   --name bar
 
 # Retrieve the connection string for the Service Bus namespace
 export SERVICE_BUS_QUEUE_CONNECTION_STRING=$(
   az servicebus namespace authorization-rule keys list \
-    --resource-group $RESOURCE_GROUP --namespace-name $SERVICE_BUS --name RootManageSharedAccessKey \
+    --resource-group $RESOURCE_GROUP --namespace-name $SERVICE_BUS_NAMESPACE --name RootManageSharedAccessKey \
     --query primaryConnectionString --output tsv
   )
 
