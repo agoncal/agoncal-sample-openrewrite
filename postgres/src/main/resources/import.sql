1cad34c8a87b6da26b796af7f393663de5b57b26
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-12-10 12:49:03 +0100

    Postgres

---
+++ postgres/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO countries(id, long_name) VALUES (1, 'The United States of America');
+INSERT INTO countries(id, long_name) VALUES (2, 'Canada');
+INSERT INTO countries(id, long_name) VALUES (3, 'Mexico');
+INSERT INTO countries(id, long_name) VALUES (4, 'Brazil');
+INSERT INTO countries(id, long_name) VALUES (5, 'Argentina');
+INSERT INTO countries(id, long_name) VALUES (6, 'Chile');
+INSERT INTO countries(id, long_name) VALUES (7, 'Peru');
+INSERT INTO countries(id, long_name) VALUES (8, 'Colombia');
+INSERT INTO countries(id, long_name) VALUES (9, 'Venezuela');
+INSERT INTO countries(id, long_name) VALUES (10, 'Uruguay');
+
+ALTER SEQUENCE countries_id_seq RESTART WITH 11;
